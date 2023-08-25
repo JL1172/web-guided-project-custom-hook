7b476fffaed77e7c67503a40537d2baf5964b6f7
Jacob Lang <jacoblang72@comcast.net>
2023-08-25 00:34:43 -0400

    wip

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export const useLocalStorage = (key,initialValue) => {
     const [storedValue, setStoredValue] = useState(()=> {
-        if (window.localStorage.getItem(key)) { //*this kinda happens upon mounting of the localstorage
+        if (window.localStorage.getItem(key)) { //*this kinda happens upon mounting of the localstorage anytime window is refreshed 
             return JSON.parse(window.localStorage.getItem(key))
         }
         window.localStorage.setItem(key,JSON.stringify(initialValue));
